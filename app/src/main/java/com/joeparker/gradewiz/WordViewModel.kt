0c76cf5525b0c82f55fb5b7eb3e72dfeddcdb6f1
Joe Parker <guyman17@yahoo.com>
2020-04-14 17:18:52 +0100

    Create ViewModel

---
+++ app/src/main/java/com/joeparker/gradewiz/WordViewModel.kt
@@ -0,0 +1,27 @@
+package com.joeparker.gradewiz
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.joeparker.gradewiz.database.AppDatabase
+import com.joeparker.gradewiz.database.entity.Word
+import com.joeparker.gradewiz.database.repository.WordRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        repository = WordRepository(AppDatabase.getDatabase(application).wordDao())
+        allWords = repository.allWords
+    }
+
+    // Non-blocking
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insertWord(word)
+    }
+}
