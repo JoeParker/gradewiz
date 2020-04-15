f78052a3c06340060e0647d36e38355f26f2ed88
Joe Parker <guyman17@yahoo.com>
2020-04-15 19:36:04 +0100

    New module activity

---
+++ app/src/main/java/com/joeparker/gradewiz/NewModuleActivity.kt
@@ -0,0 +1,42 @@
+package com.joeparker.gradewiz
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+
+class NewModuleActivity : AppCompatActivity() {
+
+    private lateinit var editModuleCodeView: EditText
+    private lateinit var editModuleNameView: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_module)
+        editModuleCodeView = findViewById(R.id.edit_module_code)
+        editModuleNameView = findViewById(R.id.edit_module_name)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editModuleNameView.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val data = arrayListOf(
+                    editModuleCodeView.text.toString(),
+                    editModuleNameView.text.toString()
+                )
+                replyIntent.putStringArrayListExtra(EXTRA_REPLY, data)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
