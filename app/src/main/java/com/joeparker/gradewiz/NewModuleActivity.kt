4878a515ae155c085d4aaed186f88c7a3c4e2fcb
Joe Parker <guyman17@yahoo.com>
2020-04-16 16:55:42 +0100

    Update module fields

--- app/src/main/java/com/joeparker/gradewiz/NewModuleActivity.kt
+++ app/src/main/java/com/joeparker/gradewiz/NewModuleActivity.kt
@@ -12,12 +12,14 @@ class NewModuleActivity : AppCompatActivity() {
 
     private lateinit var editModuleCodeView: EditText
     private lateinit var editModuleNameView: EditText
+    private lateinit var editModuleCreditsView: EditText
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_module)
         editModuleCodeView = findViewById(R.id.edit_module_code)
         editModuleNameView = findViewById(R.id.edit_module_name)
+        editModuleCreditsView = findViewById(R.id.edit_module_credits)
 
         val button = findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
@@ -27,7 +29,8 @@ class NewModuleActivity : AppCompatActivity() {
             } else {
                 val data = arrayListOf(
                     editModuleCodeView.text.toString(),
-                    editModuleNameView.text.toString()
+                    editModuleNameView.text.toString(),
+                    editModuleCreditsView.text.toString()
                 )
                 replyIntent.putStringArrayListExtra(EXTRA_REPLY, data)
                 setResult(Activity.RESULT_OK, replyIntent)
